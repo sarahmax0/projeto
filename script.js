1ac8a4addfe9b0e0553d23a33b977b0574e762ea
Saracsg <sara2006costa@gmail.com>
2023-07-10 12:53:53 -0300

    consertando um bug

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ function toggleMode() {
 
 
     //pegar a tag img
-    const img = document.querySelector("#profile img")
+    //const img = document.querySelector("#profile img")
 
     //substituir a imagem
-    if(html.classList.contains('light')) {
+    //if(html.classList.contains('light')) {
         //se tiver light mode, adicionar a imagem light
-        img.setAttribute('src', './assets/avatar.png')
-    } else {
+        //        img.setAttribute('src', './assets/avatar.png')
+    //} else {
         //se tiver sem light mode, manter a imagem normal
-        img.setAttribute('src', './assets/avatar.png')
-    }
+      //  img.setAttribute('src', './assets/avatar.png')
+    //}
 }
